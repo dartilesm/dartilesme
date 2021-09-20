fe58d58443fd5486236b3e76dedab218c9db659f
Diego Artiles <diego45882@hotmail.com>
2021-09-20 18:49:17 -0300

    feat (header): add header mobile support

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,57 +1,115 @@
-import { Row, Col, Container, Text, Link, Grid, Button } from "@nextui-org/react"
-import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi"
+import { Button, Col, Container, Grid, Link, Row, Text } from "@nextui-org/react"
+import { useState } from "react"
+import { FiGithub, FiLinkedin, FiMenu, FiTwitter, FiX } from "react-icons/fi"
 import styles from './index.module.css'
 
-const Header = () => (
-    <Grid className={styles.header}>
-        <Container>
-            <Row align="center">
-                <Col className={styles.logoContainer} span={2}>
-                    <img src="./logo3.png" alt="logo" width={50} />
-                </Col>
-                <Col className={styles.menuContainer}>
-                    <Text color="white" size="1.2rem">
-                        <Link href="#projects">
-                            Proyectos
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem">
-                        <Link href="#">
-                            Sobre mi
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem">
-                        <Link href="#">
-                            Historia
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem">
-                        <Link href="#">
-                            Tecnologías
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem" className={styles.links}>
-                        <Link href="https://github.com/dartilesm" target="_blank" rel="noreferer">
-                            <FiGithub size={20} />
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem" className={styles.links}>
-                        <Link href="https://www.linkedin.com/in/dartiles/" target="_blank" rel="noreferer">
-                            <FiLinkedin size={20} />
-                        </Link>
-                    </Text>
-                    <Text color="white" size="1.2rem" className={styles.links}>
-                        <Link href="https://twitter.com/intent/follow?screen_name=dartilesm" target="_blank" rel="noreferer">
-                            <FiTwitter size={20} />
-                        </Link>
-                    </Text>
-                    <Button auto rounded className={styles.resumeButtton}>
-                        Descargar CV
-                    </Button>
-                </Col>
-            </Row>
-        </Container>
-    </Grid>
-)
+
+const MENU_ITEMS = [
+    {
+        name: 'Proyectos',
+        href: '#projects'
+    },
+    {
+        name: 'Sobre mi',
+        href: '#'
+    },
+    {
+        name: 'Historia',
+        href: '#'
+    },
+    {
+        name: 'Tecnologías',
+        href: '#'
+    }
+]
+
+const MENU_LINKS = [
+    {
+        name: 'Github',
+        href: 'https://github.com/dartilesm',
+        icon: <FiGithub size={20} />
+    },
+    {
+        name: 'LinkedIn',
+        href: 'https://linkedin.com/in/dartiles',
+        icon: <FiLinkedin size={20} />
+    },
+    {
+        name: 'Twitter',
+        href: 'https://twitter.com/intent/follow?screen_name=dartilesm',
+        icon: <FiTwitter size={20} />
+    }
+]
+
+const Header = () => {
+    const [isOpenMenu, setIsOpenMenu] = useState(false)
+
+    return (
+        <Grid className={styles.header}>
+            <Container>
+                <Row align="center">
+                    <Col className={styles.logoContainer} span={2}>
+                        <img src="./logo3.png" alt="logo" width={40} />
+                    </Col>
+                    <Col className={styles.menuContainer}>
+                        <Grid md={0} className={styles.mobileMenuButton} onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                            <Text color="white" size="1.2rem">
+                                { isOpenMenu ? <FiX size={30} /> : <FiMenu size={30} /> }
+                            </Text>
+                        </Grid>
+                        <Grid className={`${styles.mobileMenuContainer} ${isOpenMenu ? styles.open : ''}`} md={0}>
+                            <Grid>
+                                {
+                                    MENU_ITEMS.map(({ name, href }) => 
+                                        <Text color="white" size="1.2rem" key={name} onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                                            <Link href={href}>
+                                                {name}
+                                            </Link>
+                                        </Text>
+                                    )
+                                }
+                                {
+                                    MENU_LINKS.map(({ name, icon, href }) => 
+                                        <Text color="white" size="1.2rem" className={styles.links} key={name} onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                                            <Link href={href} target="_blank" rel="noreferer">
+                                                {icon}
+                                            </Link>
+                                        </Text>
+                                    )
+                                }
+                                <Button auto rounded className={styles.resumeButtton} onClick={() => setIsOpenMenu(!isOpenMenu)}>
+                                    Descargar CV
+                                </Button>
+                            </Grid>
+                        </Grid>
+                        <Grid xs={0} md={12} className={styles.desktopMenuContainer}>
+                            {
+                                MENU_ITEMS.map(({ name, href }) => 
+                                    <Text color="white" size="1.2rem" key={name}>
+                                        <Link href={href}>
+                                            {name}
+                                        </Link>
+                                    </Text>
+                                )
+                            }
+                            {
+                                MENU_LINKS.map(({ name, icon, href }) => 
+                                    <Text color="white" size="1.2rem" className={styles.links} key={name}>
+                                        <Link href={href} target="_blank" rel="noreferer">
+                                            {icon}
+                                        </Link>
+                                    </Text>
+                                )
+                            }
+                            <Button auto rounded className={styles.resumeButtton}>
+                                Descargar CV
+                            </Button>
+                        </Grid>
+                    </Col>
+                </Row>
+            </Container>
+        </Grid>
+    )
+}
 
 export default Header
