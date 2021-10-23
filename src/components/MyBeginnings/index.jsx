ce06ef73305a3212411f56aa9923036aeb424daf
Diego Artiles <diego45882@hotmail.com>
2021-10-23 18:10:18 -0300

    feat (beginnings): create beginnings section apoc

---
+++ src/components/MyBeginnings/index.jsx
@@ -0,0 +1,27 @@
+import { Grid, Row, Spacer, Text } from "@nextui-org/react"
+
+
+const MyBeginnings = () => (
+    <>
+        <Text h2 align="center" color="white" id="projects">
+            Un poco de historia...
+        </Text>
+        <Spacer y={2} />
+        <Row justify="center">
+            <Grid sm={12} md={8} direction="column">
+            <Text color="white">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique optio omnis necessitatibus. Autem, ullam accusamus. Maxime quos quas fugiat non reprehenderit praesentium iusto odio provident dolore animi amet error, iste nihil debitis placeat voluptas. Consequatur aut reprehenderit delectus! Quod adipisci dicta, sit provident inventore, vitae eos, repellendus tempore aspernatur ipsa laborum rem natus. Aspernatur quis aut officiis, numquam quae aliquid asperiores sed iste, culpa amet accusantium corrupti magni quaerat odit saepe illum labore sint obcaecati optio architecto quasi. Minima non, voluptatem ullam sapiente aspernatur modi maxime, voluptas vel atque debitis velit. Quo, fugiat. Magni dicta debitis in reiciendis ipsa! Molestias error tenetur molestiae laborum eum itaque explicabo recusandae tempore facere temporibus placeat fugiat, aspernatur rerum quisquam consequatur, et accusamus sed?
+            </Text>
+            <Text color="white">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique optio omnis necessitatibus. Autem, ullam accusamus. Maxime quos quas fugiat non reprehenderit praesentium iusto odio provident dolore animi amet error, iste nihil debitis placeat voluptas. Consequatur aut reprehenderit delectus! Quod adipisci dicta, sit provident inventore, vitae eos, repellendus tempore aspernatur ipsa laborum rem natus. Aspernatur quis aut officiis, numquam quae aliquid asperiores sed iste, culpa amet accusantium corrupti magni quaerat odit saepe illum labore sint obcaecati optio architecto quasi. Minima non, voluptatem ullam sapiente aspernatur modi maxime, voluptas vel atque debitis velit. Quo, fugiat. Magni dicta debitis in reiciendis ipsa! Molestias error tenetur molestiae laborum eum itaque explicabo recusandae tempore facere temporibus placeat fugiat, aspernatur rerum quisquam consequatur, et accusamus sed?
+            </Text>
+            <Text color="white">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique optio omnis necessitatibus. Autem, ullam accusamus. Maxime quos quas fugiat non reprehenderit praesentium iusto odio provident dolore animi amet error, iste nihil debitis placeat voluptas. Consequatur aut reprehenderit delectus! Quod adipisci dicta, sit provident inventore, vitae eos, repellendus tempore aspernatur ipsa laborum rem natus. Aspernatur quis aut officiis, numquam quae aliquid asperiores sed iste, culpa amet accusantium corrupti magni quaerat odit saepe illum labore sint obcaecati optio architecto quasi. Minima non, voluptatem ullam sapiente aspernatur modi maxime, voluptas vel atque debitis velit. Quo, fugiat. Magni dicta debitis in reiciendis ipsa! Molestias error tenetur molestiae laborum eum itaque explicabo recusandae tempore facere temporibus placeat fugiat, aspernatur rerum quisquam consequatur, et accusamus sed?
+            </Text>
+            </Grid>
+        </Row>
+        
+    </>
+)
+
+export default MyBeginnings
