9a1e19d3dedf4bffda37492c2eebd9ca267716dd
Diego Artiles <diego45882@hotmail.com>
2022-02-01 21:14:03 -0300

    feat (profile): add profile image

--- src/containers/Presentation/index.jsx
+++ src/containers/Presentation/index.jsx
@@ -1,6 +1,48 @@
-import { Button } from "@nextui-org/react"
+import { Button, styled } from "@nextui-org/react"
 import { Section, Text, Title } from "../../components/UI"
+import MeImage from '../../assets/me.png'
 import styles from './index.module.css'
+
+const sectionStyles = {
+    height: 'calc(100vh - 100px)',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row',
+    '@smMax': {
+        flexDirection: 'column-reverse',
+        
+    }
+}
+
+const ImageContainerStyled = styled('div', {
+    height: '400px',
+    width: '400px',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center'
+})
+
+const ImageBackgroundStyled = styled('div', {
+    borderRadius: '50%',
+    background: '#060609',
+    aspectRatio: '1 / 1',
+    width: '300px',
+    height: '300px',
+    bottom: 0,
+    position: 'absolute',
+    left: '50%',
+    transform: 'translateX(-50%)',
+})
+
+const ImageStyled = styled('img', {
+    clipPath: 'ellipse(42% 46%)',
+    width: '400px',
+    height: '400px',
+    zIndex: 1,
+    objectFit: 'cover'
+})
+
 
 const Presentation = () => {
     const contact = () => {
@@ -8,7 +50,7 @@ const Presentation = () => {
     }
 
     return (
-        <Section className={styles.presentation} main>
+        <Section css={sectionStyles} main>
             <div className={styles.welcome}>
                 <Title main>
                     Bienvenido a mi portafolio
@@ -20,6 +62,10 @@ const Presentation = () => {
                     Contáctame
                 </Button>
             </div>
+            <ImageContainerStyled>
+                <ImageStyled src={MeImage} alt="me" />
+                <ImageBackgroundStyled />
+            </ImageContainerStyled>
         </Section>
     )
 }
