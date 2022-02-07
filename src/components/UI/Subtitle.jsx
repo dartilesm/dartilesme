ccfc81eae1fb0e3bd448d653f1e9562c7bb5c118
Diego Artiles <diego45882@hotmail.com>
2022-02-06 22:52:46 -0300

    style: change Title and Subtitle styles

--- src/components/UI/Subtitle.jsx
+++ src/components/UI/Subtitle.jsx
@@ -1,15 +1,19 @@
 import { styled } from '@nextui-org/react'
 
 const SubtitleStyled = styled('h3', {
+    display: 'inline-flex',
     fontSize: 'calc($base + $xs)',
     lineHeight: '$xs',
     letterSpacing: '$normal',
-    background: '-webkit-linear-gradient(-70deg, #3bf0e4 0%, #bca1f7 100%)',
+    background: 'linear-gradient(0deg, #3bf0e4 0%, #bca1f7 100%)',
     backgroundClip: 'text',
     boxDecorationBreak: 'clone',
     '-webkit-background-clip': 'text',
     '-webkit-text-fill-color': 'transparent',
     '-webkit-box-decoration-break': 'clone',
+    '-moz-background-clip': 'text',
+    '-moz-text-fill-color': 'transparent',
+    '-moz-box-decoration-break': 'clone',
     padding: '0 0 $12',
     '@smMax': {
         fontSize: '$md',
--- src/components/UI/Title.jsx
+++ src/components/UI/Title.jsx
@@ -3,19 +3,24 @@ import Divider from "./Divider";
 
 
 const commomStyles = {
+    display: 'inline-flex',
     backgroundClip: 'text',
     boxDecorationBreak: 'clone',
+    textFillColor: 'transparent',
     '-webkit-background-clip': 'text',
     '-webkit-text-fill-color': 'transparent',
     '-webkit-box-decoration-break': 'clone',
+    '-moz-background-clip': 'text',
+    '-moz-text-fill-color': 'transparent',
+    '-moz-box-decoration-break': 'clone',
     fontWeight: '$extrabold',
     lineHeight: '$sm',
     letterSpacing: '$normal',
-    padding: '0 0 $12'
+    padding: '0 0 $12',
 }
 
 const TitleStyled = styled('h2', {
-    background: '-webkit-linear-gradient(-70deg, #ff7170 0%, #ffe57f 100%)',
+    background: 'linear-gradient(0deg, #f7ff00 0%, #db36a4 100%)',
     fontSize: 'calc($sm + $lg)',
     ...commomStyles,
     '@smMax': {
@@ -30,7 +35,7 @@ const TitleStyled = styled('h2', {
 
 
 const MainTitleStyled = styled('h1', {
-    background: '-webkit-linear-gradient(-70deg, #3bf0e4 0%, #bca1f7 100%)',
+    background: 'linear-gradient(-70deg, #3bf0e4 0%, #bca1f7 100%)',
     fontSize: 'calc($xl + $base)',
     ...commomStyles,
     '@smMax': {
