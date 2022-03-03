4625e41418a65040cecc7d9b1956f10eff5100bb
Diego Artiles <diego45882@hotmail.com>
2022-03-03 18:03:44 -0300

    build: fix build

--- src/containers/Projects/Project/index.jsx
+++ src/containers/Projects/Project/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Spacer } from "@nextui-org/react"
+import { Button } from "@nextui-org/react"
 import { FiExternalLink, FiGithub } from 'react-icons/fi'
 import { ProjectButtonContainerStyled, ProjectImage, ProjectInfoContainerStyled, ProjectInfoStyled, ProjectLinkStyled, StyledProjectContainer, StyledProjectInfo } from "./styles"
 
