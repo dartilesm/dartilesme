49deed09b624a5235525f41b3a823bbf9c375dec
Diego Artiles <diego45882@hotmail.com>
2022-09-11 23:19:20 -0300

    feat (language): add language switcher

--- components/Header/Menu/index.jsx
+++ components/Header/Menu/index.jsx
@@ -1,4 +1,5 @@
-import { Button, Link, Navbar, Spacer } from "@nextui-org/react";
+import NextLink from "next/link"
+import { Button, Link, Navbar, Spacer, Dropdown } from "@nextui-org/react";
 import { useContext } from "react";
 import { FiExternalLink } from "react-icons/fi";
 import { useTranslations } from 'next-intl';
@@ -6,10 +7,13 @@ import Image from 'next/image'
 import { HashRouterContext } from "../../../context/hashRouter";
 import { Text } from "../../UI";
 import { StyledLogoContainer } from "../styles";
+import { useRouter } from "next/router";
 
 const Menu = ({ links, items }) => {
     const t = useTranslations('menu')
     const {hashLocation} = useContext(HashRouterContext)
+    const { locale, locales, route } = useRouter()
+
     return (
           <Navbar disableScrollHandler variant='sticky' maxWidth="fluid" css={{
               margin: 'auto', 
@@ -56,10 +60,37 @@ const Menu = ({ links, items }) => {
               {
                 links.map((item) => <Navbar.Link key={item.name} hideIn='sm' color='inherit' href={item.href} target="_blank"> {item.icon} </Navbar.Link>)
               }
-              <Navbar.Item>
+              <Navbar.Item show='sm' hideIn='xs'>
                 <Button auto rounded as={Link} href='cv.pdf' iconRight={<FiExternalLink color="inherit" />}>
-                  Resume
+                  {t('buttonText')}
                 </Button>
+              </Navbar.Item>
+              <Navbar.Item>
+                <Dropdown>
+                  <Dropdown.Button flat color="primary" css={{ tt: "capitalize" }}>
+                    {t(`languages.${locale}`)}
+                  </Dropdown.Button>
+                  <Dropdown.Menu
+                    aria-label="Single selection actions"
+                    color="primary"
+                    disallowEmptySelection
+                    selectionMode="single"
+                    selectedKeys={locale}
+                    css={{ $$dropdownMenuMinWidth: "100px" }}
+                  >
+                    {
+                      locales.map(_locale => {
+                        return <Dropdown.Item key={_locale}>
+                        <NextLink href={route} locale={_locale} passHref scroll={false}>
+                          <Link css={{ display: 'block', width: "100%" }}>
+                            {t(`languages.${_locale}`)}
+                          </Link>
+                        </NextLink>
+                      </Dropdown.Item>
+                      })
+                    }
+                  </Dropdown.Menu>
+                </Dropdown>
               </Navbar.Item>
             </Navbar.Content>
             <Navbar.Collapse css={{
@@ -74,40 +105,49 @@ const Menu = ({ links, items }) => {
               }
             }}>
               <li>
-              {items.map((item) => (
-                <Navbar.CollapseItem disableAnimation={false} as="span" key={item.href}>
-                  <Link
-                    color='inherit'
-                    css={{
-                      width: "100%",
-                    }}
-                    href={item.href}
-                  >
-                    {t(item.name)}
-                  </Link>
+                {
+                  items.map((item) => (
+                    <Navbar.CollapseItem as="span" key={item.href}>
+                      <Link
+                        color='inherit'
+                        css={{
+                          width: "100%",
+                        }}
+                        href={item.href}
+                      >
+                        {t(item.name)}
+                      </Link>
+                    </Navbar.CollapseItem>
+                  ))
+                }
+                <Navbar.CollapseItem as="span">
+                  <Button css={{marginTop: "$xs"}} auto rounded as={Link} href='cv.pdf' iconRight={<FiExternalLink color="inherit" />}>
+                    {t('buttonText')}
+                  </Button>
                 </Navbar.CollapseItem>
-              ))}
               </li>
               <li>
                 <Text>{t('socialText')}</Text>
-                {links.map((item) => (
-                  <Navbar.CollapseItem disableAnimation={false} as="span" key={item.href}>
-                    <Link
-                      color='inherit'
-                      css={{
-                        minWidth: "100%",
-                        display: "flex",
-                        alignItems: "center"
-                      }}
-                      href={item.href}
-                      target="_blank"
-                    >
-                        {item.icon}
-                      <Spacer x={1} />
-                        {item.name}
-                    </Link>
-                  </Navbar.CollapseItem>
-                ))}
+                {
+                  links.map((item) => (
+                    <Navbar.CollapseItem as="span" key={item.href}>
+                      <Link
+                        color='inherit'
+                        css={{
+                          minWidth: "100%",
+                          display: "flex",
+                          alignItems: "center"
+                        }}
+                        href={item.href}
+                        target="_blank"
+                      >
+                          {item.icon}
+                        <Spacer x={1} />
+                          {item.name}
+                      </Link>
+                    </Navbar.CollapseItem>
+                  ))
+                }
               </li>
             </Navbar.Collapse>
           </Navbar>
