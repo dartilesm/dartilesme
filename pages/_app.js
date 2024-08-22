b0e5f932bfbd7c4cdb09693baa9e4125c187fdfc
Diego Artiles <diego@dartiles.dev>
2024-08-21 23:26:11 -0300

    change plausible domain

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import PlausibleProvider from 'next-plausible'
 import { NextIntlProvider } from 'next-intl';
 
 function MyApp({ Component, pageProps }) {
-    return <PlausibleProvider domain='dartiles.dev'>
+    return <PlausibleProvider domain='dartiles.me'>
         <NextIntlProvider messages={pageProps.messages}>
             <Component {...pageProps} />
         </NextIntlProvider>
